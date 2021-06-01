f9adb8b6007bb37c5a2f55de0ca6fb1d81c76654
sokirko74 <sokirko@yandex.ru>
2021-06-01 12:13:49 +0300

    correct picket smil

--- sokirko_info/application/views.py
+++ sokirko_info/application/views.py
@@ -4,6 +4,9 @@ from .slide_films import SLIDE_FILMS
 
 
 def get_slide_info(urlpath):
+    _, extension = os.path.splitext(urlpath)
+    if extension == '.htm':
+        urlpath += 'l'
     if urlpath.endswith('/index.html'):
         urlpath = urlpath[:-len('/index.html')]
     key = urlpath.strip("/")
@@ -14,8 +17,12 @@ def index(request):
     template_path = request.path
     if template_path.startswith('/'):
         template_path = template_path[1:]
-    if not template_path.endswith('.html') and not template_path.endswith('.ico'):
+    if not template_path.endswith('.html') and not template_path.endswith('.ico') and not template_path.endswith('.htm'):
         template_path = os.path.join(template_path, 'index.html')
+    _, extension = os.path.splitext(template_path)
+    if extension == '.htm':
+        template_path += 'l'
+
     context = {}
     slide_film_key, slide_film_props = get_slide_info(request.path)
     if slide_film_props is not None:
