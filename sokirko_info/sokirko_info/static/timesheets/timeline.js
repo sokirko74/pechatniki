c6c9c8ff03f03cb371fc4f697a573dfb7a0ffdb1
sokirko74 <sokirko@yandex.ru>
2021-06-11 15:14:55 +0300

    prevent  hibernating

--- sokirko_info/sokirko_info/static/timesheets/timeline.js
+++ sokirko_info/sokirko_info/static/timesheets/timeline.js
@@ -93,6 +93,21 @@ function startup() {
   gMediaPlayer.addEventListener("pause", updateStartButton, false);
   gMediaPlayer.addEventListener("ended", updateStartButton, false);
   updateStartButton();
+
+  let wakeLock = null;
+
+    // Function that attempts to request a wake lock.
+    const requestWakeLock = async () => {
+      try {
+        wakeLock = await navigator.wakeLock.request('screen');
+        wakeLock.addEventListener('release', () => {
+          console.log('Wake Lock was released');
+        });
+        console.log('Wake Lock is active');
+      } catch (err) {
+        console.error(`${err.name}, ${err.message}`);
+      }
+    };
 }
 
 EVENTS.onSMILReady(function() {
