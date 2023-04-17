acd4d8bd2514b0a985848badf0c3f8d3dfc908f0
sokirko74 <sokirko@yandex.ru>
2023-04-17 21:55:36 +0300

    add yandex

--- sokirko_info/sokirko_info/urls.py
+++ sokirko_info/sokirko_info/urls.py
@@ -13,10 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os.path
+
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+from django.conf import settings
+
+def static_file(request):
+    file_path =  os.path.join(settings.STATIC_FOLDER, request.path.strip('/'))
+    with open(file_path) as inp:
+        return HttpResponse(inp.read(), content_type="text/plain")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', static_file),
+    re_path('google', static_file),
+    re_path('yandex', static_file),
     re_path('', include('application.urls')),
 ]
